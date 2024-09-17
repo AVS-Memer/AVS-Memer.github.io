b83c3fc63d82eabac7b331140abfb36b6277ed83
AVS <123315354+AVS-Memer@users.noreply.github.com>
2024-09-17 17:11:20 -0400

    Update BrainrotGenerator.js

--- Scripts/BrainrotGenerator.js
+++ Scripts/BrainrotGenerator.js
@@ -3,6 +3,7 @@ fetch("/Data/JSON/NumberBettingCalculator/brainrot.json")
   .then(res => {return res.json()})
   .then(data => Object.keys(data).forEach(type => brainrot[type] = data[type]))
   .catch(err => console.log(err));
+console.log(brainrot);
 let brainrotRandomType = (t) => {
   return brainrot[t][Math.floor(Math.random()*brainrot[t].length)];
 }
