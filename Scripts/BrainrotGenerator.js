09862ca2d1356d23e07ded080729dc2f73fc0f53
AVS <123315354+AVS-Memer@users.noreply.github.com>
2024-09-21 20:35:18 -0400

    Update BrainrotGenerator.js

--- Scripts/BrainrotGenerator.js
+++ Scripts/BrainrotGenerator.js
@@ -39,8 +39,10 @@ let randomize = () => {
     if (chosenBrainrot.person1 == chosenBrainrot.person2) {
       chosenBrainrot.person2 = "themself";
     }
-    if (brainrot.person.slice(0,6).includes(chosenBrainrot.person2)) {
+    if (brainrot.person.slice(0,4).includes(chosenBrainrot.person2)) {
       chosenBrainrot.person2 = "the " + chosenBrainrot.person2;
+    } else if (brainrot.person.slice(4,6).includes(chosenBrainrot.person2)) {
+      chosenBrainrot.person2 = "a " + chosenBrainrot.person2;
     }
     string = "A " + chosenBrainrot.adj + " " + chosenBrainrot.person1 + " " + chosenBrainrot.verbPt + " " + chosenBrainrot.person2 + " in " + chosenBrainrot.place + ".";
     if (["a", "e", "i", "o", "u"].includes(chosenBrainrot.adj[0].toLowerCase())) {
