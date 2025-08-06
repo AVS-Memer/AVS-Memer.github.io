8b1d1617baa89528f60ca1ba3a31700134967e63
AVS <123315354+AVS-Memer@users.noreply.github.com>
2025-08-06 19:47:06 -0400

    Update illionNameGenerator.js

--- Scripts/illionNameGenerator.js
+++ Scripts/illionNameGenerator.js
@@ -2,15 +2,22 @@ const basePowers = ["m", "b", "tr", "quadr", "quint", "sext", "sept", "oct", "no
 const onesPowers = ["", "un", "duo", "tre", "quattuor", "quint", "sext", "sept", "oct", "noven"];
 const tensPowers = ["", "dec", "virgint", "trigint", "quadragint", "quinquagint", "sexagint", "septuagint", "octogint", "nonagint"];
 const hundredsPowers = ["", "cent", "ducent", "trecent", "quadringent", "quingent", "sescent", "septingent", "octingent", "nongent"];
+const baseInput = document.getElementById("base");
+const coeffInput = document.getElementById("coeff");
+const expInput = document.getElementById("exp");
 
-document.getElementById("base").onchange = () => {
-  document.getElementById("coeff").max = document.getElementById("base").value;
+baseInput.onchange = () => {
+  coeffInput.max = baseInput.value;
+  expInput.max = Math.floor(3003/Math.log10(baseInput.value))-1;
 };
 
 document.getElementById("findName").onclick = () => {
-  let base = document.getElementById("base").value;
-  let coeff = document.getElementById("coeff").value;
-  let exp = document.getElementById("exp").value;
+  if (baseInput.value > baseInput.max) baseInput.value = baseInput.max;
+  if (coeffInput.value > coeffInput.max) coeffInput.value = coeffInput.max;
+  if (expInput.value > expInput.max) expInput.value = expInput.max;
+  let base = baseInput.value;
+  let coeff = coeffInput.value;
+  let exp = expInput.value;
   if (base !== 10) exp *= Math.log10(base);
   exp += Math.log10(coeff);
   if (exp < 3) document.getElementById("name").innerText = Math.pow(10,exp);
