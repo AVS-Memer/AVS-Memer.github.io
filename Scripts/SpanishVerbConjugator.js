acfb4b565c790ecb1a25cc84259907ccd7829ea4
AVS <123315354+AVS-Memer@users.noreply.github.com>
2026-03-10 21:25:38 -0400

    Update SpanishVerbConjugator.js

--- Scripts/SpanishVerbConjugator.js
+++ Scripts/SpanishVerbConjugator.js
@@ -101,5 +101,7 @@ const conjugate = (verbInput, pov, tense) => {
     ending = verbs.tense.past_participle.endings[verb.slice(-2)];
     if (ending == "ido" && ["a","e","i","o","u"].includes(stem.slice(-1))) ending = "ído";
     return stem + ending;
+  } else if (tense == "plurperfect indicative") {
+    return (reflexive?reflexive + " ":"")+conjugate("haber",pov,"imperfect indicative")+" "+conjugate(verb,null,"past participle");
   }
 }
